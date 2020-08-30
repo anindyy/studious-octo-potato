155817ce56d9e382830e794b75db6da4dae7b0a1
Anindya <pamsrewari@gmail.com>
2020-08-30 20:01:03 +0700

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # studious-octo-potato
+*please excuse the generated title*
+
+## about
+This is a simple page built to study basic html/css/js.
+
+## author
+anindya prameswari / 135 18 034
---
+++ script.js
@@ -0,0 +1,25 @@
+function handleAboutClick() {
+    document.getElementById("content").innerHTML = 
+    `
+    Hello, this is <b>Anindya</b>, <br/> <br/>
+    an informatics student who is willing to eat ice cream as breakfast,
+    lunch, and dinner every single day if they were healthy enough. <br> <br>
+    <i>(just kidding, I'll probably get sick of them on the third day)</i> <br> <br>
+    Not really fond of talking but I love making friends. 100% awkward. <br> <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <i>Do you want to get awkward together?</i>
+    `;
+}
+
+function handlePortoClick() {
+    document.getElementById("content").innerHTML = `
+    <div style="text-align: center">
+        <b style="color:#ffc08d; font-size: 24pt;">
+            <i>to be designed.</i></b> <br>
+        please wish me luck on this one!
+    </div>
+    `;
+}
